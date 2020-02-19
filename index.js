1d0db96276aa4b775d02417e8ba679d8b0cd4290
Tcrista <43964394+Tcrista@users.noreply.github.com>
2020-02-19 02:06:10 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 CLASSES = {
-  0: '~daisy',
-  1: '~dandelion',
-  2: '~roses',
-  3: '~sunflowers',
-  4: '~tulips',
+  0: 'daisy',
+  1: 'dandelion',
+  2: 'roses',
+  3: 'sunflowers',
+  4: 'tulips',
 };
 
 const MODEL_PATH =
